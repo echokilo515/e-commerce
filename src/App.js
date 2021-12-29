3543dead8bb6226ef3f4b1a23f76074c7876907e
echokilo515 <echokilo515@gmail.com>
2021-12-28 20:44:22 -1000

    upgraded react-router-dom v5 to v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import * as React from "react";
 import Homepage from "./pages/homepage/homepage.component";
+import { Route, Routes } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return <Homepage></Homepage>;
-  }
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+function App() {
+  //return <Homepage></Homepage>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route exact path="hats" element={<HatsPage></HatsPage>} />
+      </Routes>
+    </div>
+    // <Routes>
+    //   <Route>
+    //     <Route path="/" element={<Homepage />} />
+    //     {/* <Route index path="hats" children={HatsPage} /> */}
+    //   </Route>
+    // </Routes>
+  );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../menu-item/menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
     <div
       style={{ backgroundImage: `url(${imageUrl})` }}
       className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${match.link}`)}
     >
       <div
         className="background-image"
