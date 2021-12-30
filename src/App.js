0e27eb294aff413500106d55922e2566331c02a9
echokilo515 <echokilo515@gmail.com>
2021-12-30 12:16:57 -1000

    added header component and links to home/shop/contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  //return <Homepage></Homepage>;
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="shop" element={<ShopPage />} />
