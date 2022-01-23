1e06c4639cfa049badb268412a807508aa59294b
echokilo515 <echokilo515@gmail.com>
2022-01-23 11:19:42 -1000

    updated application with reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { connect } from "react-redux";
 import "./cart-dropdown.styles.scss";
 
@@ -14,8 +15,8 @@ const CartDropdown = ({ cartItems }) => (
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
